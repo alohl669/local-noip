e2f28633055e6829739b4df524de4dc0f3e29fb0
alohl669 <alohl669@p-k-dick.localdomain>
2019-06-14 16:56:46 +0200

    Mejorados los comentarios de lan-status.sh

--- lan-status.sh
+++ lan-status.sh
@@ -7,4 +7,7 @@ ping -c 1 google.com # Esto hace que el ping solo devuelva un estado
 
 # Esta linea por ahora devuelva la primera palabra de la linea que nos ha 
 # devuelto el ping
+# Queremos que sea la ultima palabra, pues cuando la conexion falla al hacer
+# ping, se nos devuelve "UNREACHABLE", los cual nos viene muy bien para
+# jugar con logica booleana
 ip addr show br0 | grep inet | awk '{ print $1; }'
