c6f5287e1d8804d91a5d0095cb772ed1ee9c0dab
alohl669 <alohl669@gmail.com>
2019-06-16 20:03:01 +0200

    corregidas rutas de equalip.sh

--- equalip.sh
+++ equalip.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ruta=/etc/local-noip
+rutaapp=$(cat /etc/local-noip/localnoip.conf)
 ipnew=$(curl ifconfig.me)
 ipold=$(cat $ruta/ipold.conf)
 
 #Comprobamos IP vieja o tomada anteriormente y de no coincidir mandamos la info y registramos la ip
 if
 	[ "$ipnew" != "$ipold"  ];
-	then sh $ruta/mailing.sh 
+	then sh $rutaapp/mailing.sh 
 	echo "$ipnew" > $ruta/ipold.conf
 fi
